fd84b8c7724e11e11c56459a94816b6f0fd23232
proninkirill <proninkirilll228@gmail.com>
2021-06-01 14:57:35 -0700

    nft_frontend

---
+++ precache-manifest.f350c62a2da89fe9ead78b384e0b0bcb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee4cbc41719556918d9eb144848bcb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3022691931accd51811f",
+    "url": "/static/css/2.be895d7a.chunk.css"
+  },
+  {
+    "revision": "8e47dca81d7dd19855da",
+    "url": "/static/css/main.2cd75d7d.chunk.css"
+  },
+  {
+    "revision": "3022691931accd51811f",
+    "url": "/static/js/2.1ecb4596.chunk.js"
+  },
+  {
+    "revision": "8e47dca81d7dd19855da",
+    "url": "/static/js/main.bdec64b6.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "46a1c0f9d6215ba723c81f0779886bfe",
+    "url": "/static/media/404.46a1c0f9.png"
+  },
+  {
+    "revision": "84faa40503c6cd93e523f338a122c126",
+    "url": "/static/media/avatar.84faa405.png"
+  },
+  {
+    "revision": "c3bb8df7b68e2be06ea62d0c920b2a8a",
+    "url": "/static/media/background.c3bb8df7.png"
+  },
+  {
+    "revision": "afee995cc3023d01ce38e9acd652ca79",
+    "url": "/static/media/connect_wallet.afee995c.png"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/static/media/metamask.c879a582.svg"
+  },
+  {
+    "revision": "40a5924f2cfa1b5507aa5f7808e29c28",
+    "url": "/static/media/multiple.40a5924f.png"
+  },
+  {
+    "revision": "8a34da977e32ae0d2304cb31d453a8ce",
+    "url": "/static/media/new1.8a34da97.gif"
+  },
+  {
+    "revision": "5b7b1c953c32835c60b66cc2f5f480ef",
+    "url": "/static/media/new2.5b7b1c95.jpg"
+  },
+  {
+    "revision": "fe41506c4b2c4ae1be8a7a95731a94e2",
+    "url": "/static/media/new3.fe41506c.gif"
+  },
+  {
+    "revision": "fd985ea4fc7cc8343ecaa8a59014c930",
+    "url": "/static/media/new4.fd985ea4.jpg"
+  },
+  {
+    "revision": "16aa110ae9c83c4d09666fefb0562d22",
+    "url": "/static/media/single.16aa110a.png"
+  },
+  {
+    "revision": "bd667eac6b507fa5fa28bc11c852cea7",
+    "url": "/static/media/user_cover.bd667eac.png"
+  }
+]);
